d3a2fb5135eaac8d77cfa7800dc150c86e37b7e8
Singgih Pratama <singgihpratama96@gmail.com>
2023-11-18 15:42:09 +0700

    setup database, logging, web

---
+++ src/application/database.js
@@ -0,0 +1,39 @@
+import {PrismaClient} from "@prisma/client";
+import {logger} from "./logging.js";
+
+export const prismaClient = new PrismaClient({
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'event',
+            level: 'error',
+        },
+        {
+            emit: 'event',
+            level: 'info',
+        },
+        {
+            emit: 'event',
+            level: 'warn',
+        },
+    ],
+});
+
+prismaClient.$on('error', (e) => {
+    logger.error(e);
+});
+
+prismaClient.$on('warn', (w) => {
+    logger.warn(w);
+});
+
+prismaClient.$on('info', (i) => {
+    logger.info(i);
+});
+
+prismaClient.$on('query', (i) => {
+    logger.info(i);
+});
